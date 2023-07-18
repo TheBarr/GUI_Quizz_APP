e3a82869757fcc95e98da8349f22b9e1263237b0
bartek <bartex600700@gmail.com>
2023-07-18 19:53:22 +0200

    ui

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 from tkinter import *
+
 THEME_COLOR = "#375362"
+
 
 class QuizInterface:
     def __init__(self):
@@ -11,15 +13,22 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250)
-        self.canvas.create_text(150,125, text="Siema", font=("Arial", 20, "italic"))
-        self.canvas.grid(row=1, column=0, columnspan=2, pady=20, padx=20)
+        self.canvas.create_text(
+            150,
+            125,
+            text="Siema",
+            font=("Arial", 20, "italic"),
+            fill=THEME_COLOR,
 
-        self.true_img = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=self.true_img)
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0, bd=0)
         self.true_button.grid(row=2, column=0)
 
-        self.false_img = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=self.false_img)
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0, bd=0)
         self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
