121468ef846ade935924161df8e57c600b6b3d13
bartek <bartex600700@gmail.com>
2023-07-18 23:51:49 +0200

    buttons, feedback

--- ui.py
+++ ui.py
@@ -6,6 +6,7 @@ THEME_COLOR = "#375362"
 
 class QuizInterface:
     def __init__(self, quiz_brain: QuizBrain):
+        self.id_after = None
         self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzy")
@@ -14,7 +15,7 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
         self.score_label.grid(row=0, column=1)
 
-        self.canvas = Canvas(width=300, height=250)
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0)
         self.question_text = self.canvas.create_text(
             150,
             125,
@@ -27,11 +28,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_img = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_img, highlightthickness=0, bd=0)
+        self.true_button = Button(image=true_img, highlightthickness=0, bd=0, command=self.green_button)
         self.true_button.grid(row=2, column=0)
 
         false_img = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_img, highlightthickness=0, bd=0)
+        self.false_button = Button(image=false_img, highlightthickness=0, bd=0, command=self.red_button)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -40,3 +41,21 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+
+    def green_button(self):
+        is_right = self.quiz.check_answer("true")
+        self.feedback(is_right)
+
+    def red_button(self):
+        is_right = self.quiz.check_answer("false")
+        self.feedback(is_right)
+
+    def feedback(self, is_right):
+        if self.id_after:
+            self.window.after_cancel(self.id_after)
+        self.id_after = self.window.after(1000, self.get_next_question)
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
